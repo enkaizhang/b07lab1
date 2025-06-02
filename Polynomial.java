ad20454052787274dd3eb7ee865e26be7909faf3
enkaizhang <ek.zhang@mail.utoronto.ca>
2025-06-01 22:50:27 -0400

    modified Polynomial.java and Driver.java

--- Polynomial.java
+++ Polynomial.java
@@ -3,6 +3,7 @@ public class Polynomial {
 	private int[] exps;
 
 	public Polynomial() {
+<<<<<<< HEAD
 		coefficients = new double[]{0.0};
 		exps = new int[]{0};
 	}
@@ -116,3 +117,43 @@ public class Polynomial {
 		return Math.abs(value) < 1e-6;
 	}
 }
+=======
+		coefficients = new double[]{0};
+	}
+	public Polynomial(double[] coeffs) {
+		this.coefficients = coeffs;
+	}
+	public Polynomial add(Polynomial second) {
+		int max_length = Math.max(this.coefficients.length, second.coefficients.length);
+		double[] new_coeffs = new double[max_length];
+		for (int i=0; i<max_length; i++) {
+			double f_coeffs, s_coeffs;
+			if (i<this.coefficients.length) {
+				f_coeffs = this.coefficients[i];
+			}
+			else {
+				f_coeffs = 0;
+			}
+			if (i<second.coefficients.length) {
+				s_coeffs = second.coefficients[i];
+			}
+			else {
+				s_coeffs = 0;
+			}
+			new_coeffs[i] = f_coeffs + s_coeffs;
+		}
+		return new Polynomial(new_coeffs);
+	}
+	public double evaluate(double x) {
+		double sum = 0;
+		for (int i=0; i<this.coefficients.length; i++) {
+			sum += coefficients[i] * Math.pow(x, i);
+		}
+		return sum;
+	}
+	public boolean hasRoot(double x) {
+		double value = evaluate(x);
+		return Math.abs(value) < 1e-6;
+	}
+}
+>>>>>>> 2d0664998b72ab517eef8766ec1f06ac98b93ca5
